63e7aebfccb265e55858a8d50294131ec22168d8
kelvyn <kudufo@gmail.com>
2023-06-28 13:32:09 +0100

    chnage the styles and added some buttons

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
-const container = document.querySelector(".container");
+const board = document.querySelector(".board");
 const hover = document.getElementById("hoverMouse");
+const userValue = document.querySelector(".userInput");
+const reset = document
+  .querySelector("#reset")
+  .addEventListener("click", createNewGrid);
+let grid = 256;
 
-for (let i = 0; i < 256; i++) {
+for (let i = 0; i < grid; i++) {
   const div2 = document.createElement("div");
-  container.appendChild(div2);
+  board.appendChild(div2);
 }
 
 hover.addEventListener("mouseover", (event) => {
   event.target.style.backgroundColor = "red";
-  
 });
 
+function createNewGrid() {
+  let userNo = userValue;
+
+  if (userNo) {
+    grid = userNo * userNo;
+    div2 = board / userNo;
+  }
+}
