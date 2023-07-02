5cbf6924d1f5dac21ab812ba6ca7b9f70f12aaac
kelvyn <kudufo@gmail.com>
2023-07-02 16:37:05 +0100

    board now working with number input to adjust the number of grid

--- app.js
+++ app.js
@@ -1,25 +1,36 @@
-const board = document.querySelector(".board");
 const hover = document.getElementById("hoverMouse");
-const userValue = document.querySelector(".userInput");
-const reset = document
-  .querySelector("#reset")
-  .addEventListener("click", createNewGrid);
-let grid = 256;
+const btn = document.querySelector(".btn");
+const board = document.querySelector(".board");
 
-for (let i = 0; i < grid; i++) {
-  const div2 = document.createElement("div");
-  board.appendChild(div2);
+let rows = 40;
+
+function populate(rows) {
+  let grid = rows * rows;
+  const board = document.querySelector(".board");
+  const size = 480;
+  board.style.width = size + "px";
+  board.style.height = size + "px";
+
+  for (let i = 0; i < grid; i++) {
+    const div2 = document.createElement("div");
+    board.appendChild(div2);
+    div2.style.width = size / rows + "px";
+    div2.style.height = size / rows + "px";
+  }
 }
 
 hover.addEventListener("mouseover", (event) => {
   event.target.style.backgroundColor = "red";
 });
+populate(rows);
 
-function createNewGrid() {
-  let userNo = userValue;
-
-  if (userNo) {
-    grid = userNo * userNo;
-    div2 = board / userNo;
+btn.addEventListener("click", (e) => {
+  let input = document.getElementById("numInput");
+  // remove children div
+  if (!input.value) {
+    console.log("no input")
+  } else {
+    board.replaceChildren();
+    populate(input.value);
   }
-}
+});
